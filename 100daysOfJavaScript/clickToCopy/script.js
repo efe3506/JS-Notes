f891ff88c7d838515ba55c81c55edb079f61f079
Efe Dallı <104692278+efe3506@users.noreply.github.com>
2022-07-02 17:21:43 +0300

    Update script.js

--- 100daysOfJavaScript/clickToCopy/script.js
+++ 100daysOfJavaScript/clickToCopy/script.js
@@ -1,19 +1,22 @@
 'use strict'
 
+//*Değişkenler
 const btn = document.querySelector('.btn');
 const copy = document.querySelector('.copy');
 
 
-
+//*eventListener ile çağırılacak fonksiyon 
 const copyText = function(e) {
+  //buttona tıklayınca sayfa yenilenmesini engellemek için preventDefault
     e.preventDefault();
    
+    //navigator tarayıcının algılanması için, clipboard API'si, tarayıcı arabirimine clipboard içeriğini okumak ve yazma özelliğini için, writeText ise belirtilen metini sisteme yazar 
     navigator.clipboard.writeText(copy.value).then(() => {
         btn.textContent = "Copied!";
         
         setTimeout(() => {
             btn.textContent = "Copy"
-        }, 2000);
+        }, 3000);
     })
 
 };
