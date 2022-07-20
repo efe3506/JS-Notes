c5af5f9468c4b1d7b234c340fbbd0b65d8bcfe22
Efe Dallı <104692278+efe3506@users.noreply.github.com>
2022-07-20 15:04:37 +0300

    Add files via upload

---
+++ 100daysOfJavaScript/submenu/script.js
@@ -0,0 +1,67 @@
+'use strict'
+
+//değişkenler
+const links = document.querySelectorAll('.nav-list li a')
+
+
+//for...of ifadesi, yinelenebilir nesneler üzerinde yinelenen bir döngü oluşturur
+for(let link of links){
+    link.addEventListener('click', smoothScroll)
+}
+
+function smoothScroll(e){
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    //scrollIntoView() yöntemi, bir öğeyi tarayıcı penceresinin görünür alanına kaydırır.
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth",
+    });
+    hideMenu();
+}
+
+
+//responsive menü
+
+//değişkenler
+const menu = document.querySelector('.nav-list');
+const menuWrapper = document.querySelector('.nav-wrapper');
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
+
+function showMenu(){
+   hamburger.style.display = 'none'; 
+   close.style.transform = 'translateY(0)';
+   menuWrapper.style.transform = 'translateX(0)';
+   menu.style.transform = 'translateX(0)';
+}
+
+function hideMenu(){
+   close.style.transform = 'translateY(-200%)';
+   hamburger.style.display = 'block';
+   menuWrapper.style.transform = 'translateX(-200%)';
+   menu.style.transform = 'translateX(200%)';
+   subMenuThree.style.transform = 'translateX(-100%)'
+}
+
+hamburger.addEventListener('click', showMenu);
+close.addEventListener('click', hideMenu);
+menuWrapper.addEventListener('click', hideMenu);
+
+//submenu section
+
+//değişkenler
+
+const thirdLink = document.querySelector('.third-link');
+const back = document.querySelector('.back-to-menu');
+const subMenuThree = document.querySelector('.submenu-three');
+
+thirdLink.addEventListener('click', () =>{
+    menu.style.transform = 'translateX(-100%)';
+    subMenuThree.style.transform = 'translateX(0)';
+});
+
+back.addEventListener('click', ()=>{
+    menu.style.transform = 'translateX(0)';
+    subMenuThree.style.transform = 'translateX(-100%)';
+})
